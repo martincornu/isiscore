de0314cad2cb3d0cbca8669079ffae67772a66a0
Martin CORNU <macornu@a106mac12-2.local>
2019-01-09 09:19:34 +0100

    first commit

---
+++ Isiscore/ScoreCell.swift
@@ -0,0 +1,14 @@
+//
+//  ScoreCell.swift
+//  Isiscore
+//
+//  Created by Martin CORNU on 09/01/2019.
+//  Copyright © 2019 Martin CORNU. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ScoreCell: UITableViewCell {
+    
+}
---
+++ Isiscore/ScoreListVC.swift
@@ -0,0 +1,15 @@
+//
+//  ScoreListVC.swift
+//  Isiscore
+//
+//  Created by Martin CORNU on 09/01/2019.
+//  Copyright © 2019 Martin CORNU. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ScoreListVC: UITableViewController {
+
+    
+}
