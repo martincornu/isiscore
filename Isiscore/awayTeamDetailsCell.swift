958552c3ac8e4cb7f5367f0d4829e4407cb45816
Martin CORNU <macornu@a106mac12-2.local>
2019-01-15 09:50:42 +0100

    home cell details layout ok ish

---
+++ Isiscore/awayTeamDetailsCell.swift
@@ -0,0 +1,14 @@
+//
+//  awayTeamDetailsCell.swift
+//  Isiscore
+//
+//  Created by Martin CORNU on 15/01/2019.
+//  Copyright © 2019 Martin CORNU. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class awayTeamDetailsCell: UITableViewCell {
+    //Outlets
+}
